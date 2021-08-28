5158fa9fe24e06e31c46447ea19084b1ef647cb8
Slavchak <slavchak968@gmail.com>
2021-08-28 14:58:38 +0300

    Initial commit

---
+++ src/com/company/Test.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayTest arrayTest = new ArrayTest();
+        arrayTest.main();
+        LinkedTest linkedTest = new LinkedTest();
+        linkedTest.main();
+    }
+}
