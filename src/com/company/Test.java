045c3852bdf93ad9a0cf4bb7ef64cb91fe35dd38
Slavchak <slavchak968@gmail.com>
2021-08-31 18:23:33 +0300

    переделал

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,68 +1,81 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Test {
+public class Test<T extends Number> {
     static final int arraySize = 100000;
 
     public static void main(String[] args) {
         final int sizeAdd = 5000;
-        final int bigSize = 100_000;
+        final int bigSize = 100000;
 
-        Test arrayTest = new Test();
-        Test linkedTest = new Test();
+        Test<Integer> arrayTest = new Test<>();
+        Test<Integer> linkedTest = new Test<>();
 
         List<Integer> arrayList = new ArrayList<>();
-        List<Integer> linkedList = new LinkedList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
 
-        arrayTest.addList(arrayList, Test.arraySize);
-        linkedTest.addList(linkedList, Test.arraySize);
+        arrayTest.addList(arrayList);
+        linkedTest.addList(linkedList);
 
-        Array(sizeAdd, arrayTest, arrayList, linkedList);
+        arrayTest.Array(sizeAdd, arrayTest, arrayList, linkedList, 15);
 
         System.out.println("--------------------------------------------------------------------");
-        Array(bigSize, arrayTest, arrayList, linkedList);
+        linkedTest.Array(bigSize, arrayTest, arrayList, linkedList, 89);
     }
 
-    private static void Array(int size, Test arrayTest, List<Integer> arrayList, List<Integer> linkedList) {
+    private void Array(int size, Test<T> arrayTest, List<T> arrayList, LinkedList<T> linkedList, T element) {
         System.out.println("Добавление " + size + " элементов в начало:");
-        System.out.println("ArrayList:  " + arrayTest.addInStart(arrayList, size));
-        System.out.println("LinkedList: " + arrayTest.addInStart(linkedList, size));
+        System.out.println("ArrayList:  " + arrayTest.addInStart(arrayList, size, element));
+        System.out.println("LinkedList: " + arrayTest.addInStart(linkedList, size, element));
 
         System.out.println("Добавление " + size + " элементов в середину:");
-        System.out.println("ArrayList:  " + arrayTest.addInMiddle(arrayList, size));
-        System.out.println("LinkedList: " + arrayTest.addInMiddle(linkedList, size));
+        System.out.println("ArrayList:  " + arrayTest.addInMiddle(arrayList, size, element));
+        System.out.println("LinkedList: " + arrayTest.addInMiddle(linkedList, size, element));
 
         System.out.println("Добавление " + size + " элементов в конец:");
-        System.out.println("ArrayList:  " + arrayTest.addInEnd(arrayList, size));
-        System.out.println("LinkedList: " + arrayTest.addInEnd(linkedList, size));
+        System.out.println("ArrayList:  " + arrayTest.addInEnd(arrayList, size, element));
+        System.out.println("LinkedList: " + arrayTest.linkAddInEnd(linkedList, size, element));
+
     }
 
-    private void addList (List <Integer> list, int arraySize) {
-        for (int i = 0; i < arraySize; i++) {
-            list.add(i);
+    private <T> void addList(Collection<T> list) {
+        for (int i = 0; i < Test.arraySize; i++) {
+            list.add((T) Integer.valueOf(i));
         }
     }
-    private long addInStart (List <Integer> list, int sizeAdd) {
+
+    private long addInStart(List<T> list, int sizeAdd, T element) {
         long time = System.nanoTime();
-        for (int i = 36; i < sizeAdd; i++) {
-            list.add(0, i);
-        }
-        return (System.nanoTime() - time);
-    }
-    private long addInMiddle (List <Integer> list, int sizeAdd) {
-        long time = System.nanoTime();
-        for (int i = 36; i < sizeAdd; i++) {
-            list.add(Test.arraySize/2, i);
+        for (int i = 0; i < sizeAdd; i++) {
+            list.add(0, element);
         }
         return (System.nanoTime() - time);
     }
-    private long addInEnd (List <Integer> list, int sizeAdd) {
+
+    private long addInMiddle(List<T> list, int sizeAdd, T element) {
         long time = System.nanoTime();
-        for (int i = 36; i < sizeAdd; i++) {
-            list.add(Test.arraySize, i);
+        for (int i = 0; i < sizeAdd; i++) {
+            list.add(Test.arraySize / 2, element);
+        }
+        return (System.nanoTime() - time);
+    }
+
+    private long addInEnd(List<T> list, int sizeAdd, T element) {
+        long time = System.nanoTime();
+        for (int i = 0; i < sizeAdd; i++) {
+            list.add(Test.arraySize, element);
+        }
+        return (System.nanoTime() - time);
+    }
+
+    private long linkAddInEnd(LinkedList<T> list, int sizeAdd, T element) {
+        long time = System.nanoTime();
+        for (int i = 0; i < sizeAdd; i++) {
+            list.addLast(element);
         }
         return (System.nanoTime() - time);
     }
